16b39539f786d6346ca933f4bec1bea25419f7c9
JopStro <joseph.strong@live.co.uk>
2024-05-05 17:26:51 +0100

    Use prelude types

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,25 @@
 use json5 as j5;
 use mlua::prelude::*;
-use serde_json;
+use serde_json::Value;
 
-fn decode<'lua>(lua: &'lua Lua, s: mlua::Value<'lua>) -> LuaResult<mlua::Value<'lua>> {
+fn decode<'lua>(lua: &'lua Lua, s: LuaValue<'lua>) -> LuaResult<LuaValue<'lua>> {
     let s = match s {
-        mlua::Value::String(ref s) => s.to_str(),
+        LuaValue::String(ref s) => s.to_str(),
         _ => Err(format!("invalid input type: {}", s.type_name())).into_lua_err(),
     }?;
-    let val: serde_json::Value = j5::from_str(s).map_err(LuaError::external)?;
+    let val: Value = j5::from_str(s).map_err(LuaError::external)?;
     lua.to_value_with(
         &val,
-        mlua::SerializeOptions::new()
+        LuaSerializeOptions::new()
             .serialize_unit_to_null(false)
             .detect_serde_json_arbitrary_precision(true),
     )
 }
 
-fn encode<'lua>(lua: &'lua Lua, v: mlua::Value<'lua>) -> LuaResult<mlua::Value<'lua>> {
-    let val: serde_json::Value = lua.from_value(v)?;
+fn encode<'lua>(lua: &'lua Lua, v: LuaValue<'lua>) -> LuaResult<LuaValue<'lua>> {
+    let val: Value = lua.from_value(v)?;
     lua.create_string(j5::to_string(&val).map_err(LuaError::external)?)
-        .map(mlua::Value::String)
+        .map(LuaValue::String)
 }
 
 #[mlua::lua_module]
